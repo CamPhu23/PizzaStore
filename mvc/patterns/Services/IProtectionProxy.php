e0dd459492f575d2cb1feb9b58f4c037eba2b36f
CamPhu23 <camphu480@gmail.com>
2021-04-06 15:16:54 +0700

    update IProtectionProxy

--- mvc/patterns/Services/IProtectionProxy.php
+++ mvc/patterns/Services/IProtectionProxy.php
@@ -5,13 +5,19 @@
         function OrderComplete();
         function CreateNewCustomerAccount();
         function EmployerManagement();
+        function CreateNewEmployerProcess();
+        function DeleteCustomerAccount();
+        function DeleteCustomerAccountProcess();
+        function SalesManagement();
+        function CreateReport();
+        function StockManagement();
+        function CreateNewCustomerAccountProcess();
+        function OrderCompleteProcess();
         function APIStockManagement_Character();
         function APIStockManagement_AsQuantity();
         function APIStockManagement_DesQuantity();
         function ShowMemberCard($fullname, $phone, $email);
         function CreateNewPost();
         function APICreateNewPostProcess();
-        function DeleteCustomerAccount();
-        function DeleteCustomerAccountProcess();
     }
 ?>  
